1be5bbd60205bb94f18ddcfc4b3b2af2c119a299
Vinicius <viniciusfontesads@gmail.com>
2024-09-28 15:46:18 -0300

    updt: tela com card de resultados

--- resources/views/admin/results/index.blade.php
+++ resources/views/admin/results/index.blade.php
@@ -10,7 +10,6 @@
             <div class="col-md-12 text-center">
                 <label class="form-label"><strong>Cursos</strong></label>
                 <div class="d-flex flex-wrap justify-content-center">
-                    
                     @foreach($courses as $course)
                     <button type="submit" name="course" value="{{ $course->course_abbreviation }}" class="btn btn-custom m-2">
                         {{ $course->course_abbreviation }}
@@ -27,74 +26,81 @@
             <div class="col-md-12 mt-3 text-center">
                 <label for="evaluative_model"><strong> Modelo Avaliativo:</strong></label>
                 <div class="d-flex justify-content-center">
-                <div class="mt-3" style="width: 250px;">
-                    <select name="evaluative_model" id="evaluative_model" class="form-control">
-                        <option value="">Selecione...</option>
-                        @foreach($evaluativeModels as $model)
-                            <option value="{{ $model->id }}" {{ request('evaluative_model') == $model->id ? 'selected' : '' }}>
-                                {{ $model->model_name }}
-                            </option>
-                        @endforeach
-                </select>
-            </div>
-        </div>
+                    <div class="mt-3" style="width: 250px;">
+                        <select name="evaluative_model" id="evaluative_model" class="form-control">
+                            <option value="">Selecione...</option>
+                            @foreach($evaluativeModels as $model)
+                                <option value="{{ $model->id }}" {{ request('evaluative_model') == $model->id ? 'selected' : '' }}>
+                                    {{ $model->model_name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
 
-        <!-- Botões de Ação -->
-        <div class="mt-3 text-center">
-            <a href="{{ route('admin.results.index') }}" class="btn btn-secondary">Limpar Filtros</a>
-            <button type="submit" class="btn btn-custom">Filtrar</button>
+                <!-- Botões de Ação -->
+                <div class="mt-3 text-center">
+                    <a href="{{ route('admin.results.index') }}" class="btn btn-secondary">Limpar Filtros</a>
+                    <button type="submit" class="btn btn-custom">Filtrar</button>
+                </div>
+            </div>
         </div>
     </form>
 
-    <!-- Tabela de Resultados -->
+    <!-- Cards de Resultados -->
     @if($works->isEmpty())
         <p>Nenhum resultado encontrado.</p>
     @else
-        @foreach ($works->groupBy('evaluative_model.model_name') as $modelName => $groupedWorks)
-            <h3 class="text-center">{{ $modelName }}</h3>
-            <table class="table table-striped table-responsive-sm">
-                <thead>
-                <tr class="text-center">
-                        <th>Ranking</th>
-                        <th>Protocolo</th>
-                        <th>Curso</th>
-                        <th>Nota Média</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <div class="row">
+            @foreach ($works->groupBy('evaluative_model.model_name') as $modelName => $groupedWorks)
+                <h2 class="text-center" style="background-color: #242025; color: white; padding: 10px;">{{ $modelName }}</h2>
                 @foreach ($groupedWorks->sortByDesc('average_score')->values() as $index => $work)
-                    <tr class="text-center">
-                        <td>
-                            @if($index == 0)
-                            <img src="/assets/ranking/icons8-1st-place-medal-emoji-32.png" alt="1º Lugar">
-                            @elseif($index == 1)
-                            <img src="/assets/ranking/icons8-2nd-place-medal-emoji-32.png" alt="2º Lugar">
-                            @elseif($index == 2)
-                            <img src="/assets/ranking/icons8-3rd-place-medal-emoji-32.png" alt="3º Lugar">
-                            @else
-                            {{ $index + 1 }}º
-                            @endif
-                        </td>
-                        <td>{{ $work->protocol }}</td>
-                        <td>{{ $work->course->course_abbreviation ?? 'N/A' }}</td>
-                        <td>{{ $work->average_score ? number_format($work->average_score, 2) : 'Sem Avaliação' }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endforeach
+                    <!--<div class="col-md-6 col-lg-4 mb-4"> Aqui mantem dois cards ou mais na mesma linha-->
+                    <div class="col-12 mb-4"> <!-- Mantendo apenas um card por coluna no desktop col-12 -->
+                        <div class="card shadow-sm">
+                            <div class="card-body text-center">
+                                <div>
+                                    @if($index == 0)
+                                    <img src="/assets/ranking/icons8-1st-place-medal-emoji-32.png" alt="1º Lugar">
+                                    @elseif($index == 1)
+                                    <img src="/assets/ranking/icons8-2nd-place-medal-emoji-32.png" alt="2º Lugar">
+                                    @elseif($index == 2)
+                                    <img src="/assets/ranking/icons8-3rd-place-medal-emoji-32.png" alt="3º Lugar">
+                                    @else
+                                    <span>{{ $index + 1 }}º</span>
+                                    @endif
+                                </div>
+                                <h5 class="card-title">{{ $work->overview }}</h5>
+                                <p class="text-muted small">Protocolo: {{ $work->protocol }} | Curso: {{ $work->course->course_name }}</p>
+                                <p class="fw-bold">Nota: {{ $work->average_score ? number_format($work->average_score, 2) : 'Sem Avaliação' }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @endforeach
+        </div>
     @endif
 </div>
+
 <style>
     .btn-custom {
-    background-color: #205483; /* Um azul mais escuro */
-    color: white; /* Cor do texto */
-}
+        background-color: #205483; /* Um azul mais escuro */
+        color: white; /* Cor do texto */
+    }
 
-.btn-custom:hover {
-    background-color: #00afef;
-    color: white;
-}
+    .btn-custom:hover {
+        background-color: #00afef;
+        color: white;
+    }
 
+    /* Estilo dos cards */
+    .card {
+        border-radius: 8px;
+        transition: transform 0.2s;
+    }
+
+    .card:hover {
+        transform: translateY(-5px);
+    }
 </style>
 @endsection
